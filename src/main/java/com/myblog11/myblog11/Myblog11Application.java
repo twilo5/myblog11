962865691bfa98c1095aa604d4023ebae2fbf8a5
shivam <ershivampandey653@gmail.com>
2024-01-05 15:11:15 +0530

    127:create save post feature

---
+++ src/main/java/com/myblog11/myblog11/Myblog11Application.java
@@ -0,0 +1,13 @@
+package com.myblog11.myblog11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog11Application.class, args);
+	}
+
+}
